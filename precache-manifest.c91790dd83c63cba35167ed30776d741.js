867b930dcccfc3fe6ff6745938e3effce7f6f3e5
AsuSoftware <antonio.asu.lucian24@gmail.com>
2020-01-30 16:59:43 +0100

    Updates

---
+++ precache-manifest.c91790dd83c63cba35167ed30776d741.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29686795aee70f813a0f46af2e1aac0d",
+    "url": "/Antonio-Lucian/index.html"
+  },
+  {
+    "revision": "d12b92ea849ade6d5fbe",
+    "url": "/Antonio-Lucian/static/css/main.e951a516.chunk.css"
+  },
+  {
+    "revision": "486ed64a5fc4f5083fc5",
+    "url": "/Antonio-Lucian/static/js/2.64ccf118.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Antonio-Lucian/static/js/2.64ccf118.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d12b92ea849ade6d5fbe",
+    "url": "/Antonio-Lucian/static/js/main.4f59724f.chunk.js"
+  },
+  {
+    "revision": "504e878786876e3f71e6",
+    "url": "/Antonio-Lucian/static/js/runtime-main.250b8af2.js"
+  },
+  {
+    "revision": "c1144107231749b75f4c3f967fd57d45",
+    "url": "/Antonio-Lucian/static/media/audi.c1144107.png"
+  },
+  {
+    "revision": "9b3259d5cc48b7dae774a9dcba281cd0",
+    "url": "/Antonio-Lucian/static/media/back-rotation.9b3259d5.jpeg"
+  },
+  {
+    "revision": "c7b5a83e7d6f1ace16a98cb8cbe84703",
+    "url": "/Antonio-Lucian/static/media/background-code.c7b5a83e.jpg"
+  },
+  {
+    "revision": "869482718a02ea987d3998947e429faf",
+    "url": "/Antonio-Lucian/static/media/certificat1.86948271.png"
+  },
+  {
+    "revision": "95d12d3a9ab1503617c18fca8c59689e",
+    "url": "/Antonio-Lucian/static/media/contact-back3.95d12d3a.jpg"
+  },
+  {
+    "revision": "b705099a0bb1051f89a8291ac894d576",
+    "url": "/Antonio-Lucian/static/media/count-down.b705099a.png"
+  },
+  {
+    "revision": "6b73c1a6d2fca9a950eab2d65b3dbf3b",
+    "url": "/Antonio-Lucian/static/media/cover-home.6b73c1a6.jpeg"
+  },
+  {
+    "revision": "81b3e7231519aff96e692c082e54106d",
+    "url": "/Antonio-Lucian/static/media/exact-time.81b3e723.png"
+  },
+  {
+    "revision": "fa8423ff0e7bbbd5f8444dc91b5117fb",
+    "url": "/Antonio-Lucian/static/media/logo-image.fa8423ff.jpg"
+  },
+  {
+    "revision": "30520fadfc8a4162a638a2b324dcd2fe",
+    "url": "/Antonio-Lucian/static/media/nasa.30520fad.jpg"
+  },
+  {
+    "revision": "6642fb74bfd6b13225dfc5a61dad4f30",
+    "url": "/Antonio-Lucian/static/media/tip-calculator.6642fb74.png"
+  },
+  {
+    "revision": "5106c490670654c9e76933e512754463",
+    "url": "/Antonio-Lucian/static/media/to-do-list.5106c490.png"
+  },
+  {
+    "revision": "c21af6b38a45edaa9855ce1463222d83",
+    "url": "/Antonio-Lucian/static/media/weather-app.c21af6b3.png"
+  }
+]);
